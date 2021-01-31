c64b9f78f6ec5b34542b56d875cfe9846c793cd5
Karthick Selvaraj <karthick@mallow-tech.com>
2021-01-31 13:14:20 +0530

    Demo logics has been updated.

--- KSTimerViewDemo/ContentView.swift
+++ KSTimerViewDemo/ContentView.swift
@@ -9,13 +9,27 @@ import SwiftUI
 import KSTimerView
 
 struct ContentView: View {
+   
     @State private var timeInterval: TimeInterval = 30
+    @State private var shouldPresentTimerView = false
+    
+    let configuration = KSTimerView.Configuration(timerBgColor: .green, timerRingBgColor: .green, actionButtonsBgColor: .blue, foregroundColor: .white, stepperValue: 10, enableLocalNotification: true, enableHapticFeedback: true)
     
     var body: some View {
-        VStack {
-            KSTimerView(timerInterval: $timeInterval, configuration: KSTimerView.Configuration(timerBgColor: .yellow, timerRingBgColor: .red, actionButtonsBgColor: .blue, foregroundColor: .white, stepperValue: 10, enableLocalNotification: true, enableHapticFeedback: true))
+        Button {
+            self.shouldPresentTimerView.toggle()
+        } label: {
+            Text("Present")
+                .foregroundColor(.white)
+                .padding()
+                .background(Color.green)
+                .clipShape(RoundedRectangle(cornerRadius: 25))
         }
+        .fullScreenCover(isPresented: $shouldPresentTimerView, content: {
+            KSTimerView(timerInterval: $timeInterval, configuration: configuration)
+        })
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
